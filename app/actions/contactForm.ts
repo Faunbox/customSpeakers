0ba3e3e8a452d4247f6005fe8e60419363e824cf
Filip Sojecki <faunbox2@gmail.com>
2025-01-15 22:18:11 +0100

    working contact form + zod validation

---
+++ app/actions/contactForm.ts
@@ -0,0 +1,97 @@
+"use server";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
+
+interface mail {
+  to: string | string[];
+  from: string;
+  subject: string;
+  text: string;
+  html: string;
+}
+
+type ResponseData = {
+  status?: string;
+  message?: string;
+};
+
+export type TemplateIdEmail = {
+  personalizations: {
+    to: string | string[];
+    dynamic_template_data: unknown;
+  };
+  from: {
+    email: string | string[];
+    name: string;
+  };
+  reply_to: { email: string | string[]; name: string };
+  template_id: string;
+};
+
+type Template = MailDataRequired | MailDataRequired[] | TemplateIdEmail;
+
+export async function sendContactEmail(formData: FormData) {
+  sgMail.setApiKey(process.env.SENDGRID_EMAIL1_API_KEY || "");
+  const name = formData.get("name")?.toString();
+  const email = formData.get("email")?.toString();
+  const description = formData.get("description");
+
+  let response: ResponseData = {};
+
+  const msgToCompany: mail = {
+    to: "kontakt@mfcustoms.pl", // Change to your recipient
+    from: process.env.SENDGRID_EMAIL!, // Change to your verified sender
+    subject: `Wiadomośc z formularza kontaktowego od ${name}`,
+    text: "data?.message",
+    html: `<div>
+    <h1>Wiadomość od: ${name}</h1>
+    <h2>Adres email: ${email}</h2>
+    <h2>Wiadomość:${description} </h2>
+    </div>`,
+  };
+
+  const customerEmail = email?.replace(/"/g, "");
+  const customerName = name?.replace(/"/g, "");
+  console.log(customerEmail);
+
+  const emailToCustomer: Template = {
+    personalizations: [
+      {
+        //@ts-expect-error:jest ok
+        to: customerEmail,
+        dynamic_template_data: {
+          name: customerName,
+        },
+      },
+    ],
+    from: {
+      email: process.env.SENDGRID_EMAIL!,
+      name: "MF Audio",
+    },
+    reply_to: {
+      email: process.env.SENDGRID_EMAIL!,
+      name: "MF Audio",
+    },
+    template_id: "d-6631ef651ed14dd2a924d1d296896f96",
+  };
+
+  await sgMail
+    .send(msgToCompany)
+    .then(() => {
+      response = {
+        status: "success",
+        message: "Message send",
+      };
+    })
+    .catch((error) => {
+      response = {
+        status: "error",
+        message: `Wstąpił błąd podczas wysyłania maila do firmy. Spróbuj ponownie później, ${error}`,
+      };
+    });
+
+  await sgMail
+  //@ts-expect-error:jest ok
+    .send(emailToCustomer)
+    .then(() => console.log("wysłano maila do klienta"));
+  return { response };
+}
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,40 +4,72 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Phone, Mail, MapPin, MessageSquareMore } from "lucide-react";
 import toast from "react-hot-toast";
+import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { sendContactEmail } from "@/app/actions/contactForm";
 
 export const email = "kontakt@mfcustoms.pl";
 export const phone_number = "790 616 496";
 export const whatsApp = phone_number.replaceAll(" ", "");
 
-const Contact = () => {
+export type ResponseData = {
+  status?: string;
+  message?: string;
+};
 
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+const validationSchema = z.object({
+  name: z.string().min(3, { message: "Imie jest wymagane" }),
+  email: z
+    .string()
+    .min(1, { message: "Adres email jest wymagany" })
+    .email({ message: "Adres email musi być poprawny" }),
+  description: z
+    .string()
+    .min(10, { message: "Wiadomość musi posiadać minimum 10 znaków" })
+    .max(300, { message: "Wiadomość musi posiadać mniej niż 300 znaków" }),
+});
+type ValidationSchema = z.infer<typeof validationSchema>;
+
+const Contact = () => {
+  const [response, setResponse] = useState<ResponseData>({
+    status: "",
     message: "",
   });
+  const [disabled, setDisabled] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const { pending } = useFormStatus();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+  });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
-  };
+  async function onSend(data: ValidationSchema) {
+    try {
+      setIsSubmitting(true);
+      const formData = new FormData();
+      Object.entries(data).forEach(([key, value]) => {
+        formData.append(key, JSON.stringify(value));
+      });
+      const res = await sendContactEmail(formData);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Here you would typically send the form data to your server or a service
-    console.log("Form submitted:", formData);
+      setResponse(res.response);
+    } catch {
+      alert("Błąd podczas wysyłania formularza");
+    } finally {
+      toast.success("Message sent successfully!");
+      setIsSubmitting(false);
+      setDisabled(true);
+      console.log(response);
+    }
+  }
 
-    // Simulate an API call
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // Show success toast
-    toast.success("Message sent successfully!");
-
-    // Reset form after submission
-    setFormData({ name: "", email: "", message: "" });
-  };
+  const onSubmit: SubmitHandler<ValidationSchema> = async (
+    data: ValidationSchema
+  ) => await onSend(data);
 
   return (
     <section id="kontakt" className="py-20 bg-gray-800">
@@ -59,7 +91,7 @@ const Contact = () => {
             viewport={{ once: true }}
           >
             <h3 className="text-2xl font-semibold mb-6">Masz pytanie?</h3>
-            <form onSubmit={handleSubmit} className="space-y-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
               <div>
                 <label htmlFor="name" className="block mb-2">
                   Imię
@@ -67,12 +99,15 @@ const Contact = () => {
                 <input
                   type="text"
                   id="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
+                  {...register("name")}
                   required
                   className="w-full px-4 py-2 rounded bg-gray-700 text-white"
                 />
+                {errors.name && (
+                  <p className="text-xs italic text-red-600 ">
+                    {errors.name?.message}
+                  </p>
+                )}
               </div>
               <div>
                 <label htmlFor="email" className="block mb-2">
@@ -81,32 +116,39 @@ const Contact = () => {
                 <input
                   type="email"
                   id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
+                  {...register("email")}
                   required
                   className="w-full px-4 py-2 rounded bg-gray-700 text-white"
                 />
+                {errors.email && (
+                  <p className="text-xs italic text-red-600 ">
+                    {errors.email?.message}
+                  </p>
+                )}
               </div>
               <div>
-                <label htmlFor="message" className="block mb-2">
+                <label htmlFor="description" className="block mb-2">
                   Wiadomość
                 </label>
                 <textarea
-                  id="message"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
+                  id="description"
+                  {...register("description")}
                   required
                   rows={4}
                   className="w-full px-4 py-2 rounded bg-gray-700 text-white"
                 ></textarea>
+                {errors.description && (
+                  <p className="text-xs italic text-red-600 ">
+                    {errors.description?.message}
+                  </p>
+                )}
               </div>
               <button
                 type="submit"
+                disabled={isSubmitting || disabled}
                 className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded transition-colors duration-300"
               >
-                Wyślij
+                {isSubmitting ? "Wysyłanie..." : "Wyślij"}
               </button>
             </form>
           </motion.div>
