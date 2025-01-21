141ff20e1ae6a92dc0fde29c9a2600ee550756d8
Filip Sojecki <faunbox2@gmail.com>
2025-01-21 21:51:12 +0100

    added google tag menager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-import { CookieConsentProvider } from './contexts/CookieConsentContext'
-import Script from 'next/script'
+import { CookieConsentProvider } from "./contexts/CookieConsentContext";
+import Script from "next/script";
 import * as Sentry from "@sentry/nextjs";
 import { CookieConsent } from "@/components/CookieConsent";
 
@@ -92,31 +92,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
-       <head>
+      <head>
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
           `}
         </Script>
       </head>
       <body className={inter.className}>
-         {/* Google Tag Manager (noscript) */}
-         <noscript>
-          <iframe 
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
+            height="0"
+            width="0"
+            //@ts-expect-error:skopiowane z google tag menager
+            style="display:none;visibility:hidden"
           ></iframe>
         </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <CookieConsentProvider>
-        {children}
-        <CookieConsent/>
+          {children}
+          <CookieConsent />
         </CookieConsentProvider>
         <Toaster
           position="bottom-right"
